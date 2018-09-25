298625bfba0b691aa4a858c9964e5c6b2204d428
Markus Frosch <markus.frosch@icinga.com>
2018-09-25 11:59:01 +0200

    Add latest tag for x86_64

--- image.mk
+++ image.mk
@@ -16,6 +16,12 @@ VERSION := $(shell basename `pwd`)
 endif
 IMAGE := $(IMAGE_PREFIX)$(VERSION):$(VARIANT)
 
+ifeq ($(VARIANT),x86_64)
+IMAGE_EXTRA := $(IMAGE_PREFIX)$(DIST):latest
+else
+IMAGE_EXTRA :=
+endif
+
 all: pull build
 
 pull:
@@ -23,10 +29,16 @@ pull:
 	docker pull "$(FROM)"
 
 build:
-	docker build --cache-from "$(IMAGE)" --tag "$(IMAGE)" .
+	docker build --cache-from "${IMAGE}" --tag "$(IMAGE)" .
+ifneq ($(IMAGE_EXTRA),)
+	docker tag "$(IMAGE)" "$(IMAGE_EXTRA)"
+endif
 
 push:
 	docker push "$(IMAGE)"
+ifneq ($(IMAGE_EXTRA),)
+	docker push "$(IMAGE_EXTRA)"
+endif
 
 clean:
 	if (docker inspect --type image "$(IMAGE)" >/dev/null 2>&1); then docker rmi "$(IMAGE)"; fi
